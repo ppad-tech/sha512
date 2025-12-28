4a3a490de1cf94ac3aafb4a5b9d07726bb8ecaee
Jared Tobin <jared@jtobin.io>
2025-12-28 17:53:44 -0330

    flake: add clang

--- flake.nix
+++ flake.nix
@@ -24,14 +24,15 @@
         lib = "ppad-sha512";
 
         pkgs = import nixpkgs { inherit system; };
-        hlib = pkgs.haskell.lib;
-        llvm = pkgs.llvmPackages_19.llvm;
+        hlib  = pkgs.haskell.lib;
+        llvm  = pkgs.llvmPackages_19.llvm;
+        clang = pkgs.llvmPackages_19.clang;
 
         base16 = ppad-base16.packages.${system}.default;
         base16-llvm =
           hlib.addBuildTools
             (hlib.enableCabalFlag base16 "llvm")
-            [ llvm ];
+            [ llvm clang ];
 
         hpkgs = pkgs.haskell.packages.ghc910.extend (new: old: {
           ppad-base16 = base16-llvm;
